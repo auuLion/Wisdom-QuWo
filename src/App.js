4e1e61942f12abe8a7deaf9a5605f970eb2616ba
lmessic <1296601488@qq.com>
2021-09-11 15:49:33 +0800

    ls-1 项目目录修改

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-// import logo from './logo.svg';
 import { Button } from 'antd';
 import './App.less';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <div>智慧曲沃建设</div>
-      <Button type="primary">按钮</Button>
+      <header className="App-header">
+        <h1>智慧曲沃建设</h1>
+        <Button type="primary">按钮</Button>
+      </header>
     </div>
   );
 }
